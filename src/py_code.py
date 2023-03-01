c5214f997de1dbc4cd568d16b65338c8f42898fd
Chekinm <120395978+Chekinm@users.noreply.github.com>
2023-03-02 02:53:56 +0300

    print compress step by step in table

--- src/py_code.py
+++ src/py_code.py
@@ -31,6 +31,18 @@ class decomp_gen_obj():
         self.generator = decompress_lzw(compressed_arr, dict_lzw_inv)
 
 
+class comp_output_table:
+
+    def __init__(self, table_ind=0):
+        self.table_ind = table_ind
+        
+    
+    def reset(self, table_ind=0):
+        self.table_ind = table_ind
+
+
+
+
 
 
 def compress(res):  # action on click  button in compress
@@ -38,27 +50,30 @@ def compress(res):  # action on click  button in compress
     if res.flag == 0:
         res.reset(Element("manual-inp").value)
         res.flag = 1
-        Element('manual-write').element.innerHTML = ""  # clear output for new string compression
+        output_table_counter.reset()
+        document.getElementById('compress-output').textContent = ''  # clear output for new string compression
         
 
     Element('compress').element.innerText = "Next step"
     try:
         output = next(res.generator)
-        if output[7] == None: 
-            Element("manual-write").write(output, append=True)
+        if output[6] == None: 
+            print_comp_table_row(output, output_table_counter)
+            
         else:
-            Element("manual-write").write(output, append=True)
-            res.dict_to_decode = output[6]
-            res.code = output[7]
+            print_last_Strings(output, output_table_counter)
+           # Element("manual-write").write(output, append=True)
+            res.dict_to_decode = output[5]
+            res.code = output[6]
             if not Element("override-inp").element.checked:
-                Element("dict").element.value = (','.join(str(i) for i in output[6]))
-                Element("code").element.value = (','.join(str(i) for i in output[7]))
+                Element("dict").element.value = (','.join(str(i) for i in output[5]))
+                Element("code").element.value = (','.join(str(i) for i in output[6]))
                 Element('decompress').remove_class ('disable')
             
     except StopIteration as e:
         Element("manual-write").write('we did it', append=True)
         #Element("manual-write").element.innerText = "we did it"
-        Element('compress').element.innerText = "Try Again"
+        Element('compress').element.innerText = "We did it! Try Again"
         res.flag = 0 
            
     return res
@@ -104,23 +119,59 @@ def check_box_action():
         Element("code").add_class ('disable')
         Element("decompress").add_class ('disable')
 
-def print_comp_table_row(out_str):
+
+
+
+def print_comp_table_row(out_str, output_table_counter):
     """construct nice css formated outout for our function
-    [1] number of current simbol
-    [2] current symbol
-    [3] current substring
-    [4] code to append
-    [5] new dict record
-    [6] it's code
-    [7] short dict to decompress appear only in the end
-    [8] compressed string code appear onlly in the end"""
+    out_srt indexes
+    [0] current symbol
+    [1] current substring
+    [2] code to append
+    [3] new dict record
+    [4] it's code
+    [5] short dict to decompress appears only in the end. type - list
+    [6] compressed string code appears only in the end,  type - list """
+    
+    ind = output_table_counter.table_ind
+
+    for i in range(ind, ind + 5):
+        td = document.createElement('div')
+        document.getElementById("compress-output").appendChild(td)
+        td.id = "tc-"+str(i)
+        td.classList.add("table-cell")
+        Element("tc-"+str(i)).write ('-' if out_str[i%5]==None else out_str[i%5])
+
+    output_table_counter.reset(ind+5) 
+
+
+def print_last_Strings(out_str, output_table_counter):
+
+    ind = output_table_counter.table_ind
+    
+    td = document.createElement('div')
+    document.getElementById("compress-output").appendChild(td)
+    td.id = "tc-"+str(ind)
+    td.classList.add("table-cell-last")
+    Element("tc-"+str(ind)).write ("Dictinary to decode: " + ','.join(str(i) for i in out_str[5]))
+
+    td = document.createElement('div')
+    document.getElementById("compress-output").appendChild(td)
+    td.id = "tc-"+str(ind+1)
+    td.classList.add("table-cell-last")
+    Element("tc-"+str(ind+1)).write ("Encoded string:" + ','.join(str(i) for i in out_str[6]))
+
+    output_table_counter.reset(ind+2) 
+
+
+
     
 
 
 
     
 
-# don't actally do without action on page. Just initialize objects in case somebody thouch buutons
+# don't actally do anyting without action on page. Just initialize some objects in case somebody thouch buutons
 
 # default object for dial with  compress 
 res = comp_gen_obj()
@@ -131,5 +182,7 @@ res.reset(Element("manual-inp").value)
 decomp = decomp_gen_obj()
 decomp.reset(res.code, res.dict_to_decode)
 
+# counters for print functions
+output_table_counter = comp_output_table(0)  
 
 
